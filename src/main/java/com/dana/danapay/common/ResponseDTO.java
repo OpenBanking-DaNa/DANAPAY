7896c37cb83c10f19749397296835de3edacb6e3
kimng1003 <kimnagyeong50@gmail.com>
2023-11-27 17:13:50 +0900

    중간커밋

---
+++ src/main/java/com/dana/danapay/common/ResponseDTO.java
@@ -0,0 +1,29 @@
+package com.dana.danapay.common;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class ResponseDTO {
+
+    private int status;          // 상태코드값
+    private String message;     // 응답 매세지
+    private Object data;        // 응답 데이터
+
+    public ResponseDTO(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public ResponseDTO(int status, String message, Object data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+}
