eb8c2e688438e7f9377ef6723fcf5260276bfc8f
kimng1003 <kimnagyeong50@gmail.com>
2023-11-27 14:14:48 +0900

    first commit

---
+++ src/main/java/com/dana/danapay/DanapayApplication.java
@@ -0,0 +1,13 @@
+package com.dana.danapay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanapayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanapayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dana/danapay/DanapayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dana.danapay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanapayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
